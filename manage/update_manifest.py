194833845ab0affbb3ff6519e460103e0159f61d
Admir Mehic <admir.mehic@upstream-mobility.net>
2023-12-08 10:26:41 +0000

    fix update_manifest / remove hacs variable

---
+++ manage/update_manifest.py
@@ -0,0 +1,27 @@
+"""Update the manifest file."""
+import sys
+import json
+import os
+
+
+def update_manifest():
+    """Update the manifest file."""
+    version = "0.0.0"
+    for index, value in enumerate(sys.argv):
+        if value in ["--version", "-V"]:
+            version = sys.argv[index + 1]
+
+    with open(
+        f"{os.getcwd()}/custom_components/wn_smartmeter/manifest.json", encoding="utf-8"
+    ) as manifestfile:
+        manifest = json.load(manifestfile)
+
+    manifest["version"] = version
+
+    with open(
+        f"{os.getcwd()}/custom_components/wn_smartmeter/manifest.json", "w", encoding="utf-8"
+    ) as manifestfile:
+        manifestfile.write(json.dumps(manifest, indent=4, sort_keys=True))
+
+
+update_manifest()
